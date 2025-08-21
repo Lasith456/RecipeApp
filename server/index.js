6c44190fcf1662a9b26ca40a6f30087866e07dfe
Lasith Chamika Viduranga <lcviduranga456@gmail.com>
2025-08-21 10:52:21 +0530

    deplyingErrorSolved

--- server/index.js
+++ server/index.js
@@ -2,30 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from 'path'; 
+import { fileURLToPath } from 'url'; 
 
 import { connectDB } from "./config/dbConnect.js";
-
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import recipeRoutes from './routes/recipeRoutes.js';
-
-import path from 'path'; 
-import { fileURLToPath } from 'url'; 
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// --- Setup for serving the frontend ---
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// 2. Point to the frontend's build folder
-app.use(express.static(path.join(__dirname, '../frontend/out'))); 
 
 app.use(express.json()); 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 connectDB();
 
@@ -33,6 +26,16 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/recipes', recipeRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../frontend/out')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/out/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
